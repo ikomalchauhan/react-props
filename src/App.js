cae9ebadf1939ba0026d0a5f440eaa9e8bdd9102
Komal Chauhan <76562738+ikomalchauhan@users.noreply.github.com>
2021-03-09 23:23:33 +0530

    Updated props with object approach

--- src/App.js
+++ src/App.js
@@ -6,28 +6,36 @@ export default function App() {
     <div className="App">
       <h1>Cat Gallery</h1>
       <ContactCard
-        name="Mr. Whiskerson"
-        imgUrl="http://placekitten.com/300/200"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+        contact={{
+          name: "Mr. Whiskerson",
+          imgUrl: "http://placekitten.com/300/200",
+          phone: "(212) 555-1234",
+          email: "mr.whiskaz@catnap.meow"
+        }}
       />
       <ContactCard
-        name="Fluffykins"
-        imgUrl="http://placekitten.com/400/200"
-        phone="(212) 555-2345"
-        email="fluff@me.com"
+        contact={{
+          name: "Fluffykins",
+          imgUrl: "http://placekitten.com/400/200",
+          phone: "(212) 555-2345",
+          email: "fluff@me.com"
+        }}
       />
       <ContactCard
-        name="Destroyer"
-        imgUrl="http://placekitten.com/400/300"
-        phone="(212) 555-3456"
-        email="ofworlds@yahoo.com"
+        contact={{
+          name: "Destroyer",
+          imgUrl: "http://placekitten.com/400/300",
+          phone: "(212) 555-3456",
+          email: "ofworlds@yahoo.com"
+        }}
       />
       <ContactCard
-        name="Felix"
-        imgUrl="http://placekitten.com/200/100"
-        phone="(212) 555-4567"
-        email="thecat@hotmail.com"
+        contact={{
+          name: "Felix",
+          imgUrl: "http://placekitten.com/200/100",
+          phone: "(212) 555-4567",
+          email: "thecat@hotmail.com"
+        }}
       />
     </div>
   );
